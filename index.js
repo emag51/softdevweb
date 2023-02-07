565d6046402a5e63d434504608678c54e9c03776
emag51 <61967786+emag51@users.noreply.github.com>
2023-02-07 22:49:18 +0700

    error page

--- index.js
+++ index.js
@@ -55,7 +55,8 @@ app.get('/logout', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    res.send('ไม่พบหน้าที่คุณร้องขอ (Error: 404 Page Not Found)')
+    // res.send('ไม่พบหน้าที่คุณร้องขอ (Error: 404 Page Not Found)')
+    res.sendFile('views/erorr.html',{root:__dirname})
 })
 
 app.listen(PORT, () => console.log(`Server Running at port ${PORT}`));
